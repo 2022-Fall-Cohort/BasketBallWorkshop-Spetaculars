5ea611826646c3bd4011e6309694d8fd597baafc
abuukar <abuukar229605@gmail.com>
2022-10-12 15:06:26 -0400

    adding style for ccs and html

---
+++ BasketBallWorkshopSpetacular/BasketBallWorkshopSpetacular/Migrations/20221012182335_Initialmigaration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasketBallWorkshopSpetacular.Migrations
+{
+    public partial class Initialmigaration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
